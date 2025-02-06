29917035ead878f73480f2a1e30bf7f2550f27cf
delta_kimigatame <sankaku.kuro@gmail.com>
2025-02-06 21:34:44 +0900

    タイムアウトの設定、テストコードがコメントアウトされていた不具合の修正

--- __tests__/Lib/GenerateFrq.test.ts
+++ __tests__/Lib/GenerateFrq.test.ts
@@ -1,6 +1,6 @@
 import { describe, test, expect } from 'vitest';
 import { World } from "tsworld";
-import Wave from "utauwav";
+import {Wave} from "utauwav";
 import fs from "fs";
 import { GenerateFrq } from "../../src/Lib/GenerateFrq";
 
@@ -8,14 +8,14 @@ describe("frqの作成", () => {
   test("とりあえず落ちなければよし", async () => {
     const world = new World();
     await world.Initialize();
-    // const buffer = fs.readFileSync("./__tests__/test_data/testbase.wav");
-    // const ab = new ArrayBuffer(buffer.length);
-    // const safeData = new Uint8Array(ab);
-    // for (let i = 0; i < buffer.length; i++) {
-    //   safeData[i] = buffer[i];
-    // }
-    // const wav = new Wave.Wave(safeData.buffer);
-    // const ndata = wav.LogicalNormalize(1) as Array<number>;
-    // const frq = GenerateFrq(world, Float64Array.from(ndata), 44100, 256);
-  });
+    const buffer = fs.readFileSync("./__tests__/test_data/testbase.wav");
+    const ab = new ArrayBuffer(buffer.length);
+    const safeData = new Uint8Array(ab);
+    for (let i = 0; i < buffer.length; i++) {
+      safeData[i] = buffer[i];
+    }
+    const wav = new Wave(safeData.buffer);
+    const ndata = wav.LogicalNormalize(1) as Array<number>;
+    const frq = GenerateFrq(world, Float64Array.from(ndata), 44100, 256);
+  }, { timeout: 30000 });
 });
