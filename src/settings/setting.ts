2d46f36ff4276a6bfc9f145eec78b31fb99807b0
delta_kimigatame <sankaku.kuro@gmail.com>
2025-02-10 06:59:24 +0900

    UTAU ICONに対する諸元設定

--- src/settings/setting.ts
+++ src/settings/setting.ts
@@ -3,16 +3,26 @@ export const productName: string = "GAKUYAweb UTAU音源パッケージ化ツー
 
 /** アプリに関する設定 */
 export const setting: { [key: string]: string } = {
-    /** アプリの名前 */
-    productName: "GAKUYAweb",
-    /** アプリのURL */
-    productUrl: "https://k-uta.jp/galuya/",
-    /** webサイトの名前 */
-    siteName: "きみがためtools",
-    /** 開発者のXアカウントのURL */
-    developerXUrl: "https://twitter.com/delta_kuro",
-    /** githubリポジトリのurl */
-    githubUrl: "https://github.com/delta-kimigatame/GakuyaWeb",
-    /** サポートdiscordのurl */
-    discordUrl: "https://discord.gg/aqZRj3CRcm",
-  };
+  /** アプリの名前 */
+  productName: "GAKUYA",
+  /** アプリのURL */
+  productUrl: "https://k-uta.jp/galuya/",
+  /** webサイトの名前 */
+  siteName: "きみがためtools",
+  /** 開発者のXアカウントのURL */
+  developerXUrl: "https://twitter.com/delta_kuro",
+  /** githubリポジトリのurl */
+  githubUrl: "https://github.com/delta-kimigatame/GakuyaWeb",
+  /** サポートdiscordのurl */
+  discordUrl: "https://discord.gg/aqZRj3CRcm",
+};
+
+/** UTAUのアイコンの諸元 */
+export const utauIcon: { [key: string]: number } = {
+  /** 横幅 */
+  width: 100,
+  /** 縦幅 */
+  height: 100,
+  /** ビット深度 */
+  depth: 3,
+};
